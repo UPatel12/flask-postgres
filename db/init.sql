fc11f69fe70dc33d9a19661a19a798e5bfa1442f
UPatel12 <30603683+UPatel12@users.noreply.github.com>
2024-06-23 19:17:21 -0400

    updating db

--- db/init.sql
+++ db/init.sql
@@ -1,7 +1,8 @@
 CREATE TABLE messages (
-    id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,s
     content TEXT NOT NULL
 );
 INSERT INTO messages (content) VALUES ('hello world');
 INSERT INTO messages (content) VALUES ('hello everyone');
 INSERT INTO messages (content) VALUES ('hello all');
+INSERT INTO messages (content) VALUES ('bye all');
