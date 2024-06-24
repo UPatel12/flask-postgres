01c1fc324d27dbb089ec47d069f53207fdf5f14c
UPatel12 <30603683+UPatel12@users.noreply.github.com>
2024-06-23 20:29:48 -0400

    updated the paths

--- db/init.sql
+++ db/init.sql
@@ -3,3 +3,6 @@ CREATE TABLE messages (
     content TEXT NOT NULL
 );
 INSERT INTO messages (content) VALUES ('hello world');
+INSERT INTO messages (content) VALUES ('hi world');
+INSERT INTO messages (content) VALUES ('sup world');
+INSERT INTO messages (content) VALUES ('bye world');
--- service2/app.py
+++ service2/app.py
@@ -9,17 +9,33 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 def get_db_connection():
     conn = psycopg2.connect(DATABASE_URL)
     return conn
+"""
+The "/" endpoint fetches the first entry in the DB
+"""
+@app.route('/')
+def get_message():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT content FROM messages LIMIT 1;')
+    message = cur.fetchone()[0]
+    cur.close()
+    conn.close()
+    return message
+
+"""
+The "/messages" endpoint fetches all the entries in the DB
+"""
 
 @app.route('/messages', methods=['GET'])
 def get_all_messages():
     conn = get_db_connection()
     cur = conn.cursor()
-
     cur.execute('SELECT * FROM messages;')
-    messages = cur.fetchone()[0]
+    messages = cur.fetchall()
     cur.close()
     conn.close()
-    return messages
+    
+    return jsonify(messages)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5002)
